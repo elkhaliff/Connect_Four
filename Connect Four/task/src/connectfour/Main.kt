3db23a5e5b156848c56073a0aecd27b6e2ad6c58
elkhaliff <elkhaliff@gmail.com>
2022-10-24 22:40:24 +0500

    Initial commit

---
+++ Connect Four/task/src/connectfour/Main.kt
@@ -0,0 +1,26 @@
+package connectfour
+
+fun main() {
+
+    fun getString(vText: String): String {
+        println(vText)
+        return readLine()!!
+    }
+
+    println("Connect Four")
+    val firstPlayer = getString("First player's name: ")
+    val secondPlayer = getString("Second player's name: ")
+    var test = false
+    val game = ConnectFour()
+    while (!test) {
+        val strBoard = getString("Set the board dimensions (Rows x Columns)\n"+
+                "Press Enter for default (6 x 7)")
+        test = game.testData(strBoard
+            .replace(" ", "")
+            .replace("\t", "")
+            .lowercase())
+    }
+
+    game.newGame(firstPlayer, secondPlayer)
+    println(game)
+}
